be75b14b652cbd8903261bbbf1fdd12ae741f6da
anillen <daniil_telenkov@vk.com>
2025-03-19 10:57:00 +0300

    remove: setter for id

--- UnitTests.Books/Models/Book.cs
+++ UnitTests.Books/Models/Book.cs
@@ -2,7 +2,7 @@ namespace UnitTests.Books.Models;
 
 public class Book(string title, string author, string publisher, DateTime publishDate)
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid Id { get; } = Guid.NewGuid();
     public string Title { get; set; } = title;
     public string Author { get; set; } = author;
     public string Publisher { get; set; } = publisher;
